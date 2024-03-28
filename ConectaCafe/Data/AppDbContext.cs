3aa4a3defff069f0e69526fbfedadcde0613f4fb
guxtagrimes <gustavolor190@gmail.com>
2024-03-28 14:24:01 -0300

    Alterações feitas

--- ConectaCafe/Data/AppDbContext.cs
+++ ConectaCafe/Data/AppDbContext.cs
@@ -17,13 +17,25 @@ namespace ConectaCafe.Data;
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Tag> Tags { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        base.OnModelCreating(modelBuilder);
+        base.OnModelCreating(builder);
 
         // FluentAPI
         #region muitos para muitos do BlogTag
-        
+        builder.Entity<BlogTag>().HasKey(
+            bt => new { bt.BlogId, bt.TagId }
+        );
+
+        builder.Entity<BlogTag>()
+            .HasOne(bt => bt.Blog)
+            .WithMany(b => b.BlogTags)
+            .HasForeignKey(bt => bt.BlogId);
+
+        builder.Entity<BlogTag>()
+            .HasOne(bt => bt.Tag)
+            .WithMany(t => t.BlogTags)
+            .HasForeignKey(bt => bt.TagId);
         #endregion
     }
 }
